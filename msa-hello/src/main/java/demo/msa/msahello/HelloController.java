a5b64f837d7dd2b3a301ecc2c65b096dcdb5ac54
zhukangfeng <zhukangfeng@gmail.com>
2018-10-30 10:48:08 +0800

    init project

---
+++ msa-hello/src/main/java/demo/msa/msahello/HelloController.java
@@ -0,0 +1,18 @@
+package demo.msa.msahello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HelloController
+ */
+@RestController
+public class HelloController {
+
+   @RequestMapping(value="/hello", method=RequestMethod.GET)
+   public String hello() {
+       return "Hello World!!!";
+   }
+    
+}
